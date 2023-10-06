403209a81964e98e4a60fb819f37f00c96ea6458
sere06001 <sere06001@utb.vaxjo.se>
2023-10-07 01:40:33 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut ur gymnasiet?");
+            Console.WriteLine("Skriv \"j\" för ja och \"n\" för nej");
+            string svar = (Console.ReadLine());
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (svar == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+            Console.ReadKey();
+        }
+    }
+}
